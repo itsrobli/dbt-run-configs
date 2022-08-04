3342d33fb22a52ee5b6863c38324e50cec18a543
Robert Li <robertwli@gmail.com>
2022-08-05 01:32:29 +1000

    First time setup of config file

--- dbt_run_configs/configs.py
+++ dbt_run_configs/configs.py
@@ -3,6 +3,8 @@ from typing import List, Optional
 from pydantic import AnyUrl, BaseModel, Field
 import yaml
 import os
+import sys
+import shutil
 
 
 class RunConfigSwitch(BaseModel):
@@ -26,7 +28,15 @@ class Configs(BaseModel):
 
 
 def parse_configs() -> Configs:
-    config_file_path = os.path.join('dbt_run_configs', 'dbt_run_config_template.yml')
-    with open(config_file_path) as config_file:
+    config_file_path_template = os.path.join('dbt_run_configs', 'dbt_run_config_template.yml')
+    config_file_path_user = os.path.join('user_space', 'dbt_run_config_user.yml')
+    if not os.path.exists(config_file_path_user):
+        print(f'First time setup.\n'
+              f'Creating {config_file_path_user}')
+        shutil.copyfile(config_file_path_template, config_file_path_user)
+        print(
+            f'Done. Edit your configs in: {config_file_path_user} and run this script again.\n')
+        sys.exit()
+    with open(config_file_path_user) as config_file:
         obj = yaml.load(config_file, Loader=yaml.FullLoader)
         return Configs.parse_obj(obj)
