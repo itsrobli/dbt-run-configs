5340cacad97e81f53bb1a1fe6053eac42ab6a5d1
Robert Li <robertwli@gmail.com>
2022-08-06 01:26:44 +1000

    Parameterise tests

--- tests/test_dbt_cli_builder.py
+++ tests/test_dbt_cli_builder.py
@@ -2,20 +2,24 @@ import pytest
 from dbt_run_configs import configs
 
 
-@pytest.fixture
-def get_test_configs() -> configs.RunConfig:
+def get_test_input_configs() -> [configs.RunConfig]:
     import os
-    return configs.parse_configs(os.path.join('.', 'test_dbt_run_config_user.yml')).run_configs[0]
+    return configs.parse_configs(
+        os.path.join('.', 'test_sample_dbt_run_config_user.yml')).run_configs
 
 
-def test_get_dbt_cli_command():
+def get_test_expected_outputs() -> [str]:
+    expected_result_1 = "dbt seed --full-refresh -m reference_mapping1 --target dev && " \
+                        "dbt snapshot -m scd_model1 scd_model2 --target dev && " \
+                        "dbt run -m model1 model2 model3+ --target dev && " \
+                        "dbt test -m model1 --target dev"
+    return [expected_result_1]
+
+
+@pytest.mark.parametrize("test_input, expected",
+                         zip(get_test_input_configs(),
+                             get_test_expected_outputs()))
+def test_get_dbt_cli_command(test_input, expected):
     from dbt_run_configs import dbt_cli_builder
-    import os
 
-    test_configs = configs.parse_configs(os.path.join('.', 'test_dbt_run_config_user.yml')).run_configs[0]
-    expected_result = "dbt seed --full-refresh -m reference_mapping1 --target dev && " \
-                      "dbt snapshot -m scd_model1 scd_model2 --target dev && " \
-                      "dbt run -m model1 model2 model3+ --target dev && " \
-                      "dbt test -m model1 --target dev"
-
-    assert dbt_cli_builder.get_dbt_cli_command(test_configs) == expected_result
+    assert dbt_cli_builder.get_dbt_cli_command(test_input) == expected
