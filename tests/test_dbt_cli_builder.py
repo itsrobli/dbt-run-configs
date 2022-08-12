963af473ffd9bac990d42bebf0270dbe83971a3c
Robert Li <robertwli@gmail.com>
2022-08-12 19:09:34 +1000

    Add more tests to parameterised tests

--- tests/test_dbt_cli_builder.py
+++ tests/test_dbt_cli_builder.py
@@ -13,7 +13,14 @@ def get_test_expected_outputs() -> [str]:
                         "dbt snapshot -m scd_model1 scd_model2 --target dev && " \
                         "dbt run -m model1 model2 model3+ --target dev && " \
                         "dbt test -m model1 --target dev"
-    return [expected_result_1]
+    expected_result_2 = "dbt seed --full-refresh --target dev && " \
+                        "dbt test -m +model1+ --target dev"
+    expected_result_3 = "dbt seed --full-refresh --target ci && " \
+                        "dbt run -m model1+ +model2+ --target ci && " \
+                        "dbt test -m +model3+ --target ci"
+    expected_result_4 = "dbt run -m config.materialized:view --target dev"
+
+    return [expected_result_1, expected_result_2, expected_result_3, expected_result_4, ]
 
 
 @pytest.mark.parametrize("test_input, expected",
