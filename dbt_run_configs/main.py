a771c2f3875bdc21bf86cad5955c2df6088f84ff
Robert Li <robertwli@gmail.com>
2022-08-19 19:48:19 +1000

    Add CLI

--- dbt_run_configs/main.py
+++ dbt_run_configs/main.py
@@ -1,11 +1,16 @@
-from dbt_run_configs import configs
+import dbt_run_configs.configs
+import dbt_cli_builder
+import click
 
 
-def print_hi(name):
-    print(configs.parse_configs())
+@click.command()
+@click.option('--name', default=None, help='Config name in user_space/dbt_run_config_user.yml.')
+def run_config(name):
+    configs = dbt_run_configs.configs.parse_configs().run_configs
+    run_config = next(filter(lambda config: config.name == name, configs))
+    run_command = dbt_cli_builder.get_dbt_cli_command(run_config)
+    print(run_command)
 
 
 if __name__ == '__main__':
-    print_hi('World')
-
-
+    run_config()
